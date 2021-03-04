bb0299830d34f98ced96c2be643084e316f04565
stri <stri@DESKTOP-TR37S0F>
2021-03-04 18:25:33 +0100

    Ajout des objets clients en paramètre des fonctions RMI startMatchmaking et StartDuel

--- src/server/main/Iserver.java
+++ src/server/main/Iserver.java
@@ -12,8 +12,8 @@ import shared.Client;
 public interface Iserver extends Remote {
 	public String login(Client client, String password) throws RemoteException, SQLException;
 	public String disconnect(Client client) throws RemoteException, SQLException;
-	public String startMatchMaking() throws RemoteException;
-	public String startDuel() throws RemoteException;
+	public String startMatchMaking(Client client) throws RemoteException;
+	public String startDuel(Client client) throws RemoteException;
 	public int register(String pseudo, String password) throws RemoteException, SQLException;
 	
 }
